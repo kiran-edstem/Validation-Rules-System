bcbde8e9bf27f039095c8e49bd9b4f98d9608806
kiran-edstem <kiran.pv@edstem.com>
2024-10-21 16:19:57 +0530

    changed the implementation - review comments (edge case - minlen > maxLen, minRange > maxRange)

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,50 @@
 abstract class ValidationRule<T> {
     abstract validate(value: T): boolean;
-    abstract getErrorMessage(): string;
+    abstract getErrorMessage(value: T): string;
 }
 
-class StringLengthRule<T extends string> implements ValidationRule<T> {
-    isValid!: boolean
-    constructor(public minLen: number, public maxLen: number) {
-    }
-    validate(value: T): boolean {
-        this.isValid = value.length >= this.minLen && value.length <= this.maxLen;
-        return this.isValid
-    }
-    getErrorMessage(): string {
-        if (this.isValid) {
-            return 'It is a valid string'
+class StringLengthRule implements ValidationRule<string> {
+    constructor(private minLen: number, private maxLen: number) {
+        if (minLen > maxLen) {
+            throw new Error("Min Length cannot be greater than max length");
         }
-        return `String length must be between ${this.minLen} and ${this.maxLen}`
+    }
+    validate(value: string): boolean {
+        return value.length >= this.minLen && value.length <= this.maxLen;
+    }
+    getErrorMessage(value: string): string {
+        if (value.length < this.minLen) {
+            return `String length must be at least ${this.minLen}`;
+        }
+        if (value.length > this.maxLen) {
+            return `String length must not exceed ${this.maxLen}`;
+        }
+        return "";
     }
 }
 
 
-class NumberRangeRule<T extends number> implements ValidationRule<T> {
-    isValid!: boolean
+class NumberRangeRule implements ValidationRule<number> {
     constructor(public minRange: number, public maxRange: number) {
     }
-    validate(value: T): boolean {
-        this.isValid = value >= this.minRange && value <= this.maxRange;
-        return this.isValid
+    validate(value: number): boolean {
+        return value >= this.minRange && value <= this.maxRange;
     }
-    getErrorMessage(): string {
-        if (this.isValid) {
-            return 'It is a valid number'
+    getErrorMessage(value: number): string {
+        if (value < this.minRange) {
+            return `Number must be at least ${this.minRange}`;
         }
-        return `Number must be between ${this.minRange} and ${this.maxRange}`
+        if (value > this.maxRange) {
+            return `Number must not exceed ${this.maxRange}`;
+        }
+        return "";
     }
 }
 
-const nameRule: StringLengthRule<string> = new StringLengthRule(2, 50);
+const nameRule: StringLengthRule = new StringLengthRule(2, 50);
 console.log(nameRule.validate("LAA"));
-console.log(nameRule.getErrorMessage());
+console.log(nameRule.getErrorMessage("LAA"));
 
-const ageRule: NumberRangeRule<number> = new NumberRangeRule(0, 150);
-console.log(ageRule.validate(-2));
-console.log(ageRule.getErrorMessage());
+const ageRule: NumberRangeRule = new NumberRangeRule(0, 150);
+console.log(ageRule.validate(2));
+console.log(ageRule.getErrorMessage(2));
